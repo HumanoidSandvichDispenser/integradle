17933630e55ec16feb7cb02946b61df81318f2db
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2022-12-06 16:50:57 -0800

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
@@ -8,20 +8,20 @@ import { Options, Vue } from "vue-class-component";
 import HelloWorld from "./components/HelloWorld.vue";
 
 @Options({
-  components: {
-    HelloWorld,
-  },
+    components: {
+        HelloWorld,
+    },
 })
 export default class App extends Vue {}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="grid">
+    </div>
+</template>
+
+<style scoped>
+</style>
