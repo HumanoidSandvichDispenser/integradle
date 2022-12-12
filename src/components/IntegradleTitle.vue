b9c9b1087c15b93b279bf0970f3f528e4f3c9ef5
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2022-12-11 22:34:44 -0800

    title and icon

--- src/components/IntegradleTitle.vue
+++ src/components/IntegradleTitle.vue
@@ -1,5 +1,21 @@
 <template>
-    <div class="title">∫INTEGRADLE</div>
+    <div class="title">
+        <div class="letters">
+            <span>I</span>
+            <span>N</span>
+            <span>T</span>
+            <span>E</span>
+            <span>G</span>
+            <span>R</span>
+            <span>A</span>
+            <span>D</span>
+            <span>L</span>
+            <span>E</span>
+        </div>
+        <div class="subtitle">
+            A daily math game
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -14,5 +30,25 @@ div.title {
     letter-spacing: 4px;
     font-size: 24px;
     font-weight: 600;
+}
+
+div.title > .letters > span {
+    display: inline-block;
+    height: 32px;
+    width: 32px;
+    font-size: 24px;
+    font-weight: 600;
+    background-color: var(--green);
+    color: var(--bg0);
+    margin: 4px;
+    text-align: center;
+    line-height: 32px;
+    letter-spacing: 0;
+}
+
+div.title > .subtitle {
+    text-transform: uppercase;
+    font-size: 16px;
+    font-weight: 500;
 }
 </style>
